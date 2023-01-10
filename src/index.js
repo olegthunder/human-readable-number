4e4b793f8200bbd3620387c3a8c9b534104882c2
olegthunder <albdgnv@gmail.com>
2023-01-11 04:38:13 +0500

    another edit

--- src/index.js
+++ src/index.js
@@ -1,190 +1,258 @@
 module.exports = function toReadable (number) {
-  let str = '';
+  let str = number.toString();
   let ReadableNumber = '';
-  str = str + number;
-  if (str.length == 3) {
-    switch (str[0]){
-        case 1: 
+    if (str.length == 1 || (str.length == 2 && str[0] == 1)) {
+            switch (str){
+        case '0': 
+        ReadableNumber = ReadableNumber + 'zero';
+        break;
+        case '1': 
+        ReadableNumber = ReadableNumber + 'one';
+        break;
+        case '2': 
+        ReadableNumber = ReadableNumber + 'two';
+        break;
+        case '3': 
+        ReadableNumber = ReadableNumber + 'three';
+        break;
+        case '4': 
+        ReadableNumber = ReadableNumber + 'four';
+        break;
+        case '5': 
+        ReadableNumber = ReadableNumber + 'five';
+        break;
+        case '6': 
+        ReadableNumber = ReadableNumber + 'six';
+        break;
+        case '7': 
+        ReadableNumber = ReadableNumber + 'seven';
+        break;
+        case '8': 
+        ReadableNumber = ReadableNumber + 'eight';
+        break;
+        case '9': 
+        ReadableNumber = ReadableNumber + 'nine';
+        break;
+        case '10': 
+        ReadableNumber = ReadableNumber + 'ten';
+        break;
+        case '11': 
+        ReadableNumber = ReadableNumber + 'eleven';
+        break;
+        case '12': 
+        ReadableNumber = ReadableNumber + 'twelve';
+        break;
+        case '13': 
+        ReadableNumber = ReadableNumber + 'thirteen';
+        break;
+        case '14': 
+        ReadableNumber = ReadableNumber + 'fourteen';
+        break;
+        case '15': 
+        ReadableNumber = ReadableNumber + 'fifteen';
+        break;
+        case '16': 
+        ReadableNumber = ReadableNumber + 'sixteen';
+        break;
+        case '17': 
+        ReadableNumber = ReadableNumber + 'seventeen';
+        break;
+        case '18': 
+        ReadableNumber = ReadableNumber + 'eighteen';
+        break;
+        case '19': 
+        ReadableNumber = ReadableNumber + 'nineteen';
+        break;
+    }
+}
+
+if (str.length == 2 && str[0] != 1) {
+            switch (str[0]){
+                case '2': 
+                ReadableNumber = ReadableNumber + 'twenty ';
+                break;
+                case '3': 
+                ReadableNumber = ReadableNumber + 'thirty ';
+                break;
+                case '4': 
+                ReadableNumber = ReadableNumber + 'forty ';
+                break;
+                case '5': 
+                ReadableNumber = ReadableNumber + 'fifty ';
+                break;
+                case '6': 
+                ReadableNumber = ReadableNumber + 'sixty ';
+                break;
+                case '7': 
+                ReadableNumber = ReadableNumber + 'seventy ';
+                break;
+                case '8': 
+                ReadableNumber = ReadableNumber + 'eighty ';
+                break;
+                case '9': 
+                ReadableNumber = ReadableNumber + 'ninety ';
+                break;
+            }
+    
+            switch (str[1]){
+                case '0':
+                ReadableNumber = ReadableNumber.slice(0,-1);
+                break;
+                case '1': 
+                ReadableNumber = ReadableNumber + 'one';
+                break;
+                case '2': 
+                ReadableNumber = ReadableNumber + 'two';
+                break;
+                case '3': 
+                ReadableNumber = ReadableNumber + 'three';
+                break;
+                case '4': 
+                ReadableNumber = ReadableNumber + 'four';
+                break;
+                case '5': 
+                ReadableNumber = ReadableNumber + 'five';
+                break;
+                case '6': 
+                ReadableNumber = ReadableNumber + 'six';
+                break;
+                case '7': 
+                ReadableNumber = ReadableNumber + 'seven';
+                break;
+                case '8': 
+                ReadableNumber = ReadableNumber + 'eight';
+                break;
+                case '9': 
+                ReadableNumber = ReadableNumber + 'nine';
+                break;
+        }
+    }
+
+if (str.length == 3 && str[1] != 1) {
+        switch (str[0]){
+        case '1': 
         ReadableNumber = 'one hundred ';
         break;
-        case 2: 
+        case '2': 
         ReadableNumber = 'two hundred ';
         break;
-        case 3: 
+        case '3': 
         ReadableNumber = 'three hundred ';
         break;
-        case 4: 
+        case '4': 
         ReadableNumber = 'four hundred ';
         break;
-        case 5: 
+        case '5': 
         ReadableNumber = 'five hundred ';
         break;
-        case 6: 
+        case '6': 
         ReadableNumber = 'six hundred ';
         break;
-        case 7: 
+        case '7': 
         ReadableNumber = 'seven hundred ';
         break;
-        case 8: 
+        case '8': 
         ReadableNumber = 'eight hundred ';
         break;
-        case 9: 
+        case '9': 
         ReadableNumber = 'nine hundred ';
         break;
     }
         switch (str[1]){
-            case 0:
+            case '0':
             ReadableNumber = ReadableNumber + '';
             break;
-            case 2: 
+            case '2': 
             ReadableNumber = ReadableNumber + 'twenty ';
             break;
-            case 3: 
+            case '3': 
             ReadableNumber = ReadableNumber + 'thirty ';
             break;
-            case 4: 
-            ReadableNumber = ReadableNumber + 'fourty ';
+            case '4': 
+            ReadableNumber = ReadableNumber + 'forty ';
             break;
-            case 5: 
+            case '5': 
             ReadableNumber = ReadableNumber + 'fifty ';
             break;
-            case 6: 
+            case '6': 
             ReadableNumber = ReadableNumber + 'sixty ';
             break;
-            case 7: 
+            case '7': 
             ReadableNumber = ReadableNumber + 'seventy ';
             break;
-            case 8: 
+            case '8': 
             ReadableNumber = ReadableNumber + 'eighty ';
             break;
-            case 9: 
+            case '9': 
             ReadableNumber = ReadableNumber + 'ninety ';
             break;
         }
 
         switch (str[2]){
-            case 0:
-            ReadableNumber.slice(0, -1);
+            case '0':
+            ReadableNumber = ReadableNumber.slice(0,-1);
             break;
-            case 1: 
+            case '1': 
             ReadableNumber = ReadableNumber + 'one';
             break;
-            case 2: 
+            case '2': 
             ReadableNumber = ReadableNumber + 'two';
             break;
-            case 3: 
+            case '3': 
             ReadableNumber = ReadableNumber + 'three';
             break;
-            case 4: 
+            case '4': 
             ReadableNumber = ReadableNumber + 'four';
             break;
-            case 5: 
+            case '5': 
             ReadableNumber = ReadableNumber + 'five';
             break;
-            case 6: 
+            case '6': 
             ReadableNumber = ReadableNumber + 'six';
             break;
-            case 7: 
+            case '7': 
             ReadableNumber = ReadableNumber + 'seven';
             break;
-            case 8: 
+            case '8': 
             ReadableNumber = ReadableNumber + 'eight';
             break;
-            case 9: 
+            case '9': 
             ReadableNumber = ReadableNumber + 'nine';
             break;
         }
-    } else if (str.length == 2 && str[0] != 1) {
-        switch (str[0]){
-            case 2: 
-            ReadableNumber = ReadableNumber + 'twenty ';
-            break;
-            case 3: 
-            ReadableNumber = ReadableNumber + 'thirty ';
-            break;
-            case 4: 
-            ReadableNumber = ReadableNumber + 'fourty ';
-            break;
-            case 5: 
-            ReadableNumber = ReadableNumber + 'fifty ';
-            break;
-            case 6: 
-            ReadableNumber = ReadableNumber + 'sixty ';
-            break;
-            case 7: 
-            ReadableNumber = ReadableNumber + 'seventy ';
-            break;
-            case 8: 
-            ReadableNumber = ReadableNumber + 'eighty ';
-            break;
-            case 9: 
-            ReadableNumber = ReadableNumber + 'ninety ';
-            break;
-        }
+}
 
-        switch (str[1]){
-            case 0:
-            ReadableNumber.slice(0, -1);
-            break;
-            case 1: 
-            ReadableNumber = ReadableNumber + 'one';
-            break;
-            case 2: 
-            ReadableNumber = ReadableNumber + 'two';
-            break;
-            case 3: 
-            ReadableNumber = ReadableNumber + 'three';
-            break;
-            case 4: 
-            ReadableNumber = ReadableNumber + 'four';
-            break;
-            case 5: 
-            ReadableNumber = ReadableNumber + 'five';
-            break;
-            case 6: 
-            ReadableNumber = ReadableNumber + 'six';
-            break;
-            case 7: 
-            ReadableNumber = ReadableNumber + 'seven';
-            break;
-            case 8: 
-            ReadableNumber = ReadableNumber + 'eight';
-            break;
-            case 9: 
-            ReadableNumber = ReadableNumber + 'nine';
-            break;
-    }
-  } else {
+if (str.length == 3 && str[1] == 1) {
     switch (str[0]){
-        case 0: 
-        ReadableNumber = ReadableNumber + 'zero';
-        case 1: 
-        ReadableNumber = ReadableNumber + 'one';
+        case '1': 
+        ReadableNumber = 'one hundred ';
         break;
-        case 2: 
-        ReadableNumber = ReadableNumber + 'two';
+        case '2': 
+        ReadableNumber = 'two hundred ';
         break;
-        case 3: 
-        ReadableNumber = ReadableNumber + 'three';
+        case '3': 
+        ReadableNumber = 'three hundred ';
         break;
-        case 4: 
-        ReadableNumber = ReadableNumber + 'four';
+        case '4': 
+        ReadableNumber = 'four hundred ';
         break;
-        case 5: 
-        ReadableNumber = ReadableNumber + 'five';
+        case '5': 
+        ReadableNumber = 'five hundred ';
         break;
-        case 6: 
-        ReadableNumber = ReadableNumber + 'six';
+        case '6': 
+        ReadableNumber = 'six hundred ';
         break;
-        case 7: 
-        ReadableNumber = ReadableNumber + 'seven';
+        case '7': 
+        ReadableNumber = 'seven hundred ';
         break;
-        case 8: 
-        ReadableNumber = ReadableNumber + 'eight';
+        case '8': 
+        ReadableNumber = 'eight hundred ';
         break;
-        case 9: 
-        ReadableNumber = ReadableNumber + 'nine';
+        case '9': 
+        ReadableNumber = 'nine hundred ';
         break;
+    }
+    switch (number % 100){
         case 10: 
         ReadableNumber = ReadableNumber + 'ten';
         break;
@@ -215,7 +283,8 @@ module.exports = function toReadable (number) {
         case 19: 
         ReadableNumber = ReadableNumber + 'nineteen';
         break;
+    }
 }
-  }
-  return ReadableNumber;
+return ReadableNumber;
 }
+
